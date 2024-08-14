dec01515e43ca194a4fe3dca6a87b87a3dbf9b31
Anonymushadow <83378179+Anonymushadow@users.noreply.github.com>
2024-08-14 01:59:30 -0300

    Delete Guia Completa/6_Variables_globales directory

--- Guia Completa/6_Variables_globales/datos.cjs
+++
@@ -1,8 +0,0 @@
-var localVariable = "Soy una variable local";
-global.globalVariable = "Soy una variable global";
-
-function myFunction() {
-  console.log(localVariable); // Puedes acceder a la variable local aquí
-}
-
-module.exports = myFunction;
