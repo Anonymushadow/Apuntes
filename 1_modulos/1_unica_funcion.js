34aa29a3b43f91648e309a4184b1b9b3781ad1b7
Anonymushadow <83378179+Anonymushadow@users.noreply.github.com>
2024-08-14 01:45:16 -0300

    Delete 1_modulos directory

--- 1_modulos/1_unica_funcion.js
+++
@@ -1,6 +0,0 @@
-function saludar(nombre){
-    return `Hola ${nombre}`;
-}
-
-//module.exports.nombre_que_le_voy_a_asignar = valor_a_exportar;
-module.exports.saludar = saludar;
--- 1_modulos/5_os.js
+++
@@ -1,9 +0,0 @@
-const OS = require("os");
-
-console.log("os.type(): " + OS.type());
-console.log("os.homedir: " + OS.homedir);
-console.log("os.uptime(): " + OS.uptime());
-console.log("os.userInfo(): " + OS.userInfo());
-
-
-
